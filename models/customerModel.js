722f30d549432497722344cfb1fff053994c4c34
Shyam Sunder <shyamp665@gmail.com>
2022-10-04 23:30:35 +0530

    add customer model

---
+++ models/customerModel.js
@@ -0,0 +1,26 @@
+const { boolean } = require('joi');
+const { default: mongoose } = require('mongoose');
+const mongooose = require('mongoose');
+
+
+const customerScheme = new mongoose.Schema({
+    name :{
+        type:String,
+        required: true,
+        minlength: 5,
+        maxlength : 255,
+    },
+    isGold : {
+        type: Boolean,
+        required: true,
+    },
+    phone:{
+        type: String,
+        required: true,
+        length:10
+    },
+});
+
+const Customer = new mongooose.model("Customer", customerScheme);
+
+module.export = Customer;
