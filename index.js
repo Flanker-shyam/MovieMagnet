3254e8bb08f6c80bf14f4201a5c91aa31cd3701e
Shyam Sunder <shyamp665@gmail.com>
2022-10-04 11:35:47 +0530

    add movies model

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ const joi = require('joi');
 const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const helmet = require('helmet');
+const connectDB = require("./databaseConnect/mongooseDB");
 
 const port = process.env.PORT || 3001;
 const app = express();
 
 app.use(helmet());
 
+
+connectDB("mongodb://localhost:27017/moviesData");
 
 app.listen(port, (err)=>{
     if(err)
