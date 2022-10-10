af325b9d53055cf5353416ff7b84098b6082e923
Shyam Sunder <shyamp665@gmail.com>
2022-10-10 14:26:22 +0530

    add  validators

--- models/moviesModel.js
+++ models/moviesModel.js
@@ -1,25 +1,29 @@
 const mongoose = require('mongoose');
+const Genre = require("./genreModel");
+const validators = require("../validators/index");
 
 const moviesSchema = new mongoose.Schema({
-    name : {
-        type:String,
-        require : true,
-        minlength : 5,
-        lowercase:true
+    name: {
+        type: String,
+        require: true,
+        minlength: 5,
+        lowercase: true
     },
     tags:{
-        type:Array,
-        validate:{
-            validator: function(v){
-                return (v && v.length>0);
-            },
-            message : "Need atleast one value in the tags",
+        type: Array,
+        validate: {
+            validator: validators.validateTags,
+            message: "Need at least one value in the tags",
         }
     },
     category:{
         type: String,
-        require : true
+        require: true
     },
+    genre: {
+        type: String,
+        require: true
+    }
 });
 
 const Movies = new mongoose.model("Movies", moviesSchema);
