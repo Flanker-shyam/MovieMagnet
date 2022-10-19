cce77b48a74f71b1bda3138c83383f249903c10a
Shyam Sunder <shyamp665@gmail.com>
2022-10-19 17:19:38 +0530

    fixed bug in Fawn

--- routes/rentalRoute.js
+++ routes/rentalRoute.js
@@ -9,15 +9,15 @@ const Fawn = require("fawn");
 const rental = require("../models/rentalModel");
 const Customer = require("../models/customerModel");
 const Movie = require("../models/moviesModel");
-const Movies = require("../models/moviesModel");
+// const Movies = require("../models/moviesModel");
 
 router.use(cors());
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(helmet());
 
-const URI = process.env.DB_URL;
+// const URI = process.env.DB_URL;
 
-Fawn.init(URI);
+// Fawn.init(URI);
 
 router.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
@@ -34,8 +34,13 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+    // console.log(Movie);
     const customer = await Customer.findById(req.body.customerId);
+    console.log(req.body.movieId);
     const movie = await Movie.findById(req.body.movieId);
+
+    console.log(customer);
+    console.log(movie);
     if (!customer || !movie) {
         return res.status(400).send("invalid customer or Movie");
     }
@@ -49,19 +54,20 @@ router.post("/", async (req, res) => {
             dailyRentalrate: movie.dailyRentalrate
         },
         customer: {
-            _id: customer._Id,
+            _id: customer._id,
             name: customer.name,
             phone: customer.phone
         }
     });
 
     try {
-        new Fawn.Task()
-            .save("rentals", newRental)
-            .update("movies", {_id:movie._id},{ $inc: {numberInStock: -1}})
-            .run();
+        const result = await newRental.save();
+        // new Fawn.Task()
+        //     .save("rentals", newRental)
+        //     .update("movies", {_id:movie._id},{ $inc: {numberInStock: -1}})
+        //     .run();
 
-            res.send(newRental);
+            res.send(result);
     }
     catch(exp)
     {
