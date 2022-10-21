f504770286629fdd6c1d5f7a764e7abc1fedf1ea
Shyam Sunder <shyamp665@gmail.com>
2022-10-21 23:37:33 +0530

    Added transaction in rentalRoute.js

--- routes/rentalRoute.js
+++ routes/rentalRoute.js
@@ -5,19 +5,14 @@ const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
 var cors = require('cors');
 const helmet = require("helmet");
-const Fawn = require("fawn");
 const rental = require("../models/rentalModel");
 const Customer = require("../models/customerModel");
 const Movie = require("../models/moviesModel");
-// const Movies = require("../models/moviesModel");
 
 router.use(cors());
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(helmet());
 
-// const URI = process.env.DB_URL;
-
-// Fawn.init(URI);
 
 router.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
@@ -34,13 +29,10 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-    // console.log(Movie);
+
     const customer = await Customer.findById(req.body.customerId);
-    console.log(req.body.movieId);
     const movie = await Movie.findById(req.body.movieId);
 
-    console.log(customer);
-    console.log(movie);
     if (!customer || !movie) {
         return res.status(400).send("invalid customer or Movie");
     }
@@ -60,19 +52,31 @@ router.post("/", async (req, res) => {
         }
     });
 
-    try {
-        const result = await newRental.save();
-        // new Fawn.Task()
-        //     .save("rentals", newRental)
-        //     .update("movies", {_id:movie._id},{ $inc: {numberInStock: -1}})
-        //     .run();
+    //Transactions of mongodb
 
-            res.send(result);
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+    try {
+        const addedMovie = await rental.create([newRental],{session});
+        const foundMovie = await Movie.findOne({ _id: movie._id }, null, { session });
+        console.log(foundMovie);
+
+        foundMovie.numberInStock = foundMovie.numberInStock-1;
+
+        await foundMovie.save();
+
+        await session.commitTransaction();
+        res.send(addedMovie);
     }
-    catch(exp)
-    {
-        res.status(500).send("Error occurred in fawn ops");
+    catch (e) {
+        await session.abortTransaction();
+        res.send(e.message);
+        console.error(e);
+    } finally {
+        session.endSession();
     }
 });
 
 module.exports = router;
+
