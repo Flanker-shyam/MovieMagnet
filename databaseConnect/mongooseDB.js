74ea7144801d2b740163acb2e2f1cc2b671b65bb
Shyam Sunder <85950516+Flanker-shyam@users.noreply.github.com>
2024-07-09 16:41:38 +0000

    remove url from db logs

--- databaseConnect/mongooseDB.js
+++ databaseConnect/mongooseDB.js
@@ -5,7 +5,7 @@ const connectDB = (url)=>{
     mongoose
     .connect(url)
     .then(()=>{
-        console.log(`Database connected successfully to ${url}...`);
+        console.log(`Database connected successfully...`);
     })
     .catch((err)=>{
         console.log(`Error while connecting to database: ${err}`);
